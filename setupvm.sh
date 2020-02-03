711402c005e1e309f991acf9140e3dbdfa7c977f
Andre <andrewvrosa@gmail.com>
2020-02-03 05:28:18 -0800

    Bash script to setup vm for C/C++ programming

---
+++ setupvm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# setup ubuntu vm for c programming
+# author: Andre Rosa
+# date: 03FEB2020
+
+apt update
+apt install gcc
+apt install g++
+apt install vim
+apt install make
+apt install net-tools
+apt install openssh-server
